3906ed92bdbd65b60b1c5fdcae717690b71cefab
johnmasson <john.masson@gmail.com>
2015-07-22 10:18:48 +0100

    better return data types for poll results and event subscriptions

---
+++ ElucidatClient/Models/PollResultsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElucidatClient.Models {
+    public class PollResultsModel {
+        public string ReleaseCode { get; set; }
+        public PollResultsAnswers Answers { get; set; }
+    }
+
+    public class PollResultsAnswers {
+        public IDictionary<string, IDictionary<string, int>> Summary { get; set; }
+        public IDictionary<string, IDictionary<string, IDictionary<string, int>>> ReleaseVersions { get; set; } 
+    }
+}
